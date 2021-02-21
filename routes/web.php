ba14755ae38ec25cac55c6a3f3aba9a6391293a3
190103479 <190103479@stu.sdu.edu.kz>
2021-02-21 16:40:13 +0600

    creation of routes to pages

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,19 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+
+
+Route::get('/portfolio', function () {
+    return view('portfolio');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
 });
