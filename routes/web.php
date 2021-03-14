940b12a0cab755af859399cf2e1fe4b3f284f026
190103479 <190103479@stu.sdu.edu.kz>
2021-03-14 23:23:55 +0600

    create blogs view to create

--- routes/web.php
+++ routes/web.php
@@ -44,3 +44,9 @@ Route::get('/post/create', function () {
 });
 
 Route::get('/post', [BlogController::class, 'index']);
+
+Route::get('post/create', function(){
+    return view('blog.create');
+});
+
+Route::post('post/create', [BlogController::class,'store'])->name('add-blog');
